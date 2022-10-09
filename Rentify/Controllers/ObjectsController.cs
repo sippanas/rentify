c871dce8a9dd24fc22f1f0cda09600cdce987df2
sippanas <alavicius.lukas@gmail.com>
2022-10-09 19:31:45 +0300

    Finished Objects controller

--- Rentify/Controllers/ObjectsController.cs
+++ Rentify/Controllers/ObjectsController.cs
@@ -34,13 +34,70 @@ namespace Rentify.Controllers
         }
 
         // GET: /api/object-types/1/objects/2
-        [HttpGet("{id}")]
+        [HttpGet("{objectId}")]
         public async Task<ActionResult<ObjectDto>> Get(int objectTypeId, int objectId)
         {
             var selectedObject = await _objectsRepository.Get(objectTypeId, objectId);
             if (selectedObject == null) return NotFound();
 
-            return Ok(selectedObject);
+            var selectedObjectDto = new ObjectDto(selectedObject.Id, selectedObject.Address,
+                selectedObject.Price, selectedObject.RelevantInformation, selectedObject.ObjectTypeId);
+
+            return Ok(selectedObjectDto);
+        }
+
+        // POST: /api/object-types/{objectTypeId}/objects
+        [HttpPost]
+        public async Task<ActionResult<Data.Models.Object>> Post(int objectTypeId, CreateObjectDto createObjectDto)
+        {
+            var objectType = await _objectsTypesRepository.Get(objectTypeId);
+            if(objectType == null) return NotFound();
+
+            var newObject = new Data.Models.Object
+            {
+                Address = createObjectDto.Address,
+                Price = createObjectDto.Price,
+                RelevantInformation = createObjectDto.RelevantInformation,
+                ObjectTypeId = objectTypeId
+            };
+
+            await _objectsRepository.Create(newObject);
+
+            return Created($"/api/object-types/{objectTypeId}/objects/{newObject.Id}", newObject);
+        }
+
+        // PUT: /api/object-types/1/objects/2
+        [HttpPut("{objectId}")]
+        public async Task<ActionResult<Data.Models.Object>> Put(int objectTypeId, int objectId, UpdateObjectDto updateObjectDto)
+        {
+            var objectType = await _objectsTypesRepository.Get(objectTypeId);
+            if (objectType == null) return NotFound();
+
+            var updatableObject = await _objectsRepository.Get(objectTypeId, objectId);
+            if (updatableObject == null) return NotFound();
+
+            updatableObject.Address = updateObjectDto.Address;
+            updatableObject.Price = updateObjectDto.Price;
+            updatableObject.RelevantInformation = updateObjectDto.RelevantInformation;
+
+            await _objectsRepository.Put(updatableObject);
+
+            return Ok(updatableObject);
+        }
+
+        // DELETE: /api/object-types/1/objects/2
+        [HttpDelete("{objectId}")]
+        public async Task<ActionResult<Data.Models.Object>> Delete(int objectTypeId, int objectId)
+        {
+            var objectType = await _objectsTypesRepository.Get(objectTypeId);
+            if (objectType == null) return NotFound();
+
+            var removableObject = await _objectsRepository.Get(objectTypeId, objectId);
+            if (removableObject == null) return NotFound();
+
+            await _objectsRepository.Delete(removableObject);
+
+            return NoContent();
         }
     }
 }
--- Rentify/Data/Dtos/ObjectDtos.cs
+++ Rentify/Data/Dtos/ObjectDtos.cs
@@ -1,4 +1,8 @@
-﻿namespace Rentify.Data.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rentify.Data.Dtos
 {
     public record ObjectDto(int Id, string? Address, int Price, string? RelevantInformation, int ObjectTypeId);
+    public record CreateObjectDto([Required] string Address, int Price, string? RelevantInformation);
+    public record UpdateObjectDto([Required] string Address, int Price, string? RelevantInformation);
 }
