6476f24c22ccb27cb6c99d4eabc343c23d44c6c9
sippanas <alavicius.lukas@gmail.com>
2022-10-14 20:16:43 +0300

    Updated RoomDtos

--- Rentify/Data/Dtos/RoomDtos.cs
+++ Rentify/Data/Dtos/RoomDtos.cs
@@ -3,6 +3,10 @@
 namespace Rentify.Data.Dtos
 {
     public record RoomDto(int Id, string? Name, int Size, int ObjectId);
-    public record CreateRoomDto([Required] string? Name, int Size);
-    public record UpdateRoomDto([Required] string? Name, int Size);
+    public record CreateRoomDto(
+        [Required] string? Name, 
+        [Range(1, 1000)] int Size);
+    public record UpdateRoomDto(
+        [Required] string? Name, 
+        [Range(1, 1000)] int Size);
 }
