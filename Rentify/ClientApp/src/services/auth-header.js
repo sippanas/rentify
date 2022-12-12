88c7944c4d3cc0a3a108cbe403e2b31109618434
sippanas <alavicius.lukas@gmail.com>
2022-12-13 00:51:16 +0200

    Moved auth stuff as a service

---
+++ Rentify/ClientApp/src/services/auth-header.js
@@ -0,0 +1,10 @@
+﻿export default function authHeader() {
+    const user = JSON.parse(localStore.getItem("user"));
+
+    if (user && user.accessToken) {
+        return { "x-auth-token": user.accessToken };
+    }
+    else {
+        return {};
+    }
+}
---
+++ Rentify/ClientApp/src/services/auth.service.js
@@ -0,0 +1,42 @@
+﻿import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+
+const contentHeader = new Headers({ 'content-type': 'application/json' });
+
+const register = (name, surname, email, password) => {
+    return axios
+        .post('api/register', { name, surname, email, password }, {
+            headers: contentHeader
+        });
+};
+
+const login = (email, password) => {
+    return axios
+        .post('api/login', { email, password }, {
+            headers: contentHeader
+        })
+        .then((response) => {
+            if (response.data.accessToken) {
+                localStorage.setItem("user", JSON.stringify(response.data));
+            }
+
+            return response;
+        })
+};
+
+const logout = () => {
+    localStorage.removeItem("user");
+};
+
+const getCurrentUser = () => {
+    return localStorage.getItem("user");
+};
+
+const authService = {
+    register,
+    login,
+    logout,
+    getCurrentUser
+}
+
+export default authService;
