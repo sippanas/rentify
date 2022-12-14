bd2dc1831ff41d41bc48449cc5393646a5088d81
sippanas <alavicius.lukas@gmail.com>
2022-12-14 21:15:37 +0200

    Added Footer to Layout

---
+++ Rentify/ClientApp/src/components/Footer.js
@@ -0,0 +1,16 @@
+﻿import React, { Component } from 'react';
+
+export class Footer extends Component {
+    static displayName = Footer.name;
+
+    render() {
+        const date = new Date();
+        return (
+            <div className="container">
+                <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                    <p className="col-md-4 mb-0 text-muted">&copy; {date.getFullYear()} Rentify</p>
+                </footer>
+            </div>
+        );
+    }
+}
